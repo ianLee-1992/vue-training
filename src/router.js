2a2583b9cb09c7448ccfb7084b1370578d169483
ian <502219968@qq.com>
2018-08-28 23:48:06 +0800

    step-02 新闻资讯组件初步

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
+import NewsList from './components/news/NewsList.vue'
 
 // 3. 创建路由对象
 const router = new VueRouter({
@@ -13,7 +14,8 @@ const router = new VueRouter({
     { path: '/home', component: HomeContainer },
     { path: '/member', component: MemberContainer },
     { path: '/shopcar', component: ShopcarContainer },
-    { path: '/search', component: SearchContainer }
+    { path: '/search', component: SearchContainer },
+    { path: '/home/newslist', component: NewsList }
   ],
   linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类，默认的类叫 router-link-active
 })
