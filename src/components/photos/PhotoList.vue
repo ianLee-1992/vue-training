78cb61dcd2544c7a709634f10462e5d83aff9173
ian <502219968@qq.com>
2018-08-29 23:33:54 +0800

    step-07 图片列表的渲染

--- src/components/photos/PhotoList.vue
+++ src/components/photos/PhotoList.vue
@@ -5,27 +5,24 @@
     <div id="slider" class="mui-slider">
       <div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
         <div class="mui-scroll">
-          <a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
-            推荐
-          </a>
-          <a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
-            热点
-          </a>
-          <a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
-            北京
-          </a>
-          <a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
-            社会
-          </a>
-          <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-            娱乐
-          </a>
-          <a class="mui-control-item" href="#item6mobile" data-wid="tab-top-subpage-6.html">
-            科技
+          <a :class="['mui-control-item', item.id == 0 ? ' mui-active' : '']" v-for="item in cates" :key="item.id" @click="getPhotoListByCateIdTest(item.id)">
+            {{ item.title }}
           </a>
         </div>
       </div>
     </div>
+
+    <!-- 图片列表区域 -->
+    <ul class="photo-list">
+      <li v-for="item in list" :key="item.id">
+        <img v-lazy="item.img_url">
+        <div class="info">
+          <h3 class="info-title">{{ item.title }}</h3>
+          <div class="info-body">{{ item.zhaiyao }}</div>
+        </div>
+      </li>
+    </ul>
+
   </div>
 </template>
 
@@ -36,7 +33,51 @@ import mui from '../../lib/mui/js/mui.min.js'
 
 export default {
   data() {
-    return {}
+    return {
+      cates: [  // 所有分类的列表数组
+        {
+          title: '全部',
+          id: 0
+        },
+        {
+          title: '家居生活',
+          id: 14
+        },
+        {
+          title: '摄影设计',
+          id: 15
+        },
+        {
+          title: '汽车资讯',
+          id: 16
+        },
+        {
+          title: '互联网科技',
+          id: 17
+        },
+        {
+          title: '体育新闻',
+          id: 18
+        },
+        {
+          title: '电影大片',
+          id: 19
+        },
+        {
+          title: '时尚娱乐',
+          id: 20
+        }
+      ],
+      list: [ // 图片列表的数组
+        
+      ]
+    }
+  },
+  created() {
+    // this.getAllCategory()
+    // 默认进入页面，就主动请求 所有图片列表的数据
+    // this.getPhotoListByCateId(0)
+    this.getPhotoListByCateIdTest()
   },
   mounted() { 
     // 当组件中的 DOM 结构被渲染好并放到页面中后，会执行这个钩子函数
@@ -46,7 +87,73 @@ export default {
       deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
     })
   },
-  methods: {}
+  methods: {
+    getAllCategory() {
+      // 获取所有的图片分类
+      this.$http.get('api/getimgcategory').then(result => {
+        if (result.body.status === 0) {
+          // 手动拼接出一个最完整的 分类列表
+          result.body.message.unshift({ title: '全部', id: 0 })
+          this.cates = result.body.message
+        }
+      })
+    },
+    getPhotoListByCateId(cateId) {
+      // 根据分类 id 获取图片列表
+      this.$http
+        .get('api/getimages/' + cateId)
+        .then(result => {
+          if (result.body.status === 0) {
+            this.list = result.body.message
+          }
+        })
+    },
+    getPhotoListByCateIdTest() {
+      // 本地数据测试
+      let list = [
+        {
+          id: 50,
+          title: '那年、那些年',
+          img_url: 'src/images/d4.jpg',
+          zhaiyao: '空间再有限，生活也要有一定的仪式感，找到让人可以惬意的栖身之地。'
+        },
+        {
+          id: 51,
+          title: '那年我们...',
+          img_url: 'src/images/d5.jpg',
+          zhaiyao: '空间再有限，生活也要有一定的仪式感，找到让人可以惬意的栖身之地。'
+        },
+        {
+          id: 52,
+          title: '那年我们...',
+          img_url: 'src/images/d6.jpg',
+          zhaiyao: '空间再有限，生活也要有一定的仪式感，找到让人可以惬意的栖身之地。'
+        },
+        {
+          id: 53,
+          title: '那年我们...',
+          img_url: 'src/images/d7.jpg',
+          zhaiyao: '空间再有限，生活也要有一定的仪式感，找到让人可以惬意的栖身之地。'
+        },
+        {
+          id: 54,
+          title: '那年我们...',
+          img_url: 'src/images/d8.jpg',
+          zhaiyao: '空间再有限，生活也要有一定的仪式感，找到让人可以惬意的栖身之地。'
+        },
+        {
+          id: 55,
+          title: '那年我们...',
+          img_url: 'src/images/d3.jpg',
+          zhaiyao: '空间再有限，生活也要有一定的仪式感，找到让人可以惬意的栖身之地。'
+        }
+      ]
+      this.list = list
+      console.log(this.list);
+      
+    }
+
+  }
 }
 </script>
 
@@ -54,4 +161,42 @@ export default {
 * {
   touch-action: pan-y;
 }
+.photo-list {
+  list-style: none;
+  margin: 0;
+  padding: 10px 10px 0;
+  li {
+    background-color: #ccc;
+    text-align: center;
+    margin-bottom: 10px;
+    box-shadow: 0 0 9px #999;
+    position: relative;
+    img {
+      width: 100%;
+      vertical-align: middle;
+    }
+    img[lazy=loading] {
+      width: 40px;
+      height: 300px;
+      margin: auto;
+    }
+
+    .info {
+      color: #fff;
+      text-align: left;
+      position: absolute;
+      bottom: 0;
+      background-color: rgba($color: #000, $alpha: 0.4);
+      max-height: 88px;
+      .info-title {
+        font-size: 14px;
+      }
+      .info-body {
+        font-size: 12px;
+      }
+    }
+  }
+}
+
+
 </style>
--- src/index.js
+++ src/index.js
@@ -29,11 +29,15 @@ import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
 
 // 按需导入 Mint-UI 中的组件
-import { Header, Swipe, SwipeItem, Button } from 'mint-ui';
+/* import { Header, Swipe, SwipeItem, Button, Lazyload } from 'mint-ui';
 Vue.component(Header.name, Header)
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
 Vue.component(Button.name, Button)
+Vue.use(Lazyload) */
+import MintUI from 'mint-ui'
+Vue.use(MintUI)
+import 'mint-ui/lib/style.css'
 
 // 导入自己的 router.js 路由模块
 import router from './router'
