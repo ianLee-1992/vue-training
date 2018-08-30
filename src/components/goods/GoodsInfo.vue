fe807e591607bdb829b5476fbcdf53658483c25b
ian <502219968@qq.com>
2018-08-30 22:42:50 +0800

    购物车小球效果及购买数量组件数量监听

--- src/components/goods/GoodsInfo.vue
+++ src/components/goods/GoodsInfo.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="goodsinfo-container">
+
+    <transition
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter">
+      <div class="ball" v-show="ballFlag" ref="ball"></div>
+    </transition>
     
     <!-- 商品轮播图区域 -->
     <div class="mui-card">
@@ -19,10 +26,15 @@
             市场价: <span><del>¥{{ goodsinfo.market_price }}.00</del></span>
             销售价: <span class="now_price">¥{{ goodsinfo.sell_price }}.00</span>
           </p>
-          <p>购买数量: <numbox></numbox></p>
+          <p>购买数量: <numbox @getcount="getSelectedCount" :max="goodsinfo.stock_quantity"></numbox></p>
           <p>
             <mt-button type="primary" size="small">立即购买</mt-button>
-            <mt-button type="danger" size="small">加入购物车</mt-button>
+            <mt-button type="danger" size="small" @click="addToShopCar">加入购物车</mt-button>
+            <!-- 分析: 如何实现加入购物车时候, 拿到 选择的数量 -->
+            <!-- 1. 按钮属于 goodsinfo 页面, 数字 属于 numberbox 组件 -->
+            <!-- 2. 由于涉及到了父子组件的嵌套了, 所以无法直接在 goodsinfo 页面中获取到 选中的商品数量值 -->
+            <!-- 3. 涉及到了 子组件向父组件传值(事件调用机制) -->
+            <!-- 4. 事件调用本质: 父向子传递方法, 子调用这个方法, 同时把数据当作参数,传递给这个方法 -->
           </p>
         </div>
       </div>
@@ -65,12 +77,14 @@ export default {
           src: 'src/images/mac1.jpg'
         }
       ],
+      ballFlag: false,  // 控制小球隐藏和显示的标识符
+      selectedCount: 1, // 保存用户选中的商品数量(默认选中购买1个)
       goodsinfo: {  // 获取到的商品的信息
         id: 38,
         title: '苹果（Apple） 2018新款MacBook Pro 15.4英寸苹果笔记本电脑 2017款 灰色/256G MR932CH/A 2018款 Multi-Touch Bar',
         add_time: '2022-02-04T12:12:12.000Z',
         goods_no: 'MR942CH/A',
-        stock_quantity: 666,
+        stock_quantity: 66,
         market_price: 17288,
         sell_price: 17238
       }
@@ -113,6 +127,43 @@ export default {
       // 点击跳转至 商品评论页面
       this.$router.push({ name: 'goodscomment', params: { id } })
     },
+    addToShopCar() {
+      // 添加到购物车
+      this.ballFlag = !this.ballFlag
+    },
+    beforeEnter(el) {
+      el.style.transform = "translate(0, 0)"
+    },
+    enter(el, done) {
+      el.offsetWidth
+
+      // 小球动画优化思路：
+      // 1. 先分析导致 动画不准确的本质原因: 我们把小球最终位移到的位置,已经局限在了某一分辨率下的,滚动条未滚动的情况下;
+      // 2. 只要分辨率和测试的时候不一样,或者滚动条有一定的滚动距离之后,问题就出现了;
+      // 3. 因此,我们经过分析,得到结论: 不能把位移的横纵坐标写死,而是应该根据不同情况,动态计算这个坐标值
+      // 4. 经过分析,思路: 先得到 徽标的横纵坐标, 再得到小球的横纵坐标, 然后 让 y 值求差, x 值也求差, 得到的结果, 就横纵坐标要位移的距离
+      // 5. 如何 获取 徽标和小球的位置? domObject.getBoundingClientRect()
+
+      // 获取小球 在页面中的位置
+      const ballPosition = this.$refs.ball.getBoundingClientRect()
+      // 获取徽标 在页面中的位置
+      const badgePosition = document.querySelector('#badge').getBoundingClientRect()
+
+      const xDist = badgePosition.left - ballPosition.left
+      const yDist = badgePosition.top - ballPosition.top
+
+      el.style.transform = `translate(${xDist}px, ${yDist}px)`
+      el.style.transition = "all .5s cubic-bezier(.4, -0.3, 1, .68)"
+      done()
+    },
+    afterEnter(el) {
+      this.ballFlag = !this.ballFlag
+    },
+    getSelectedCount(count) {
+      // 当子组件把 选中的数量传递给父组件的时候, 把选中的值保存到 data 上
+      this.selectedCount = count
+      console.log('父组件拿到的数量值为: ' + this.selectedCount);
+    },
     getLunbotuLocal() {
       this.lunbotu.forEach(item => {
         item.img = item.src
@@ -142,6 +193,17 @@ export default {
     button {
       margin: 15px 0;
     }
+  }
+
+  .ball {
+    position: absolute;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: #f23030;
+    z-index: 99;
+    top: 28rem;
+    left: 9rem;
   }
 }
 </style>
--- src/components/subcomponents/goodsinfo_numbox.vue
+++ src/components/subcomponents/goodsinfo_numbox.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="mui-numbox" data-numbox-min='1' data-numbox-max='9'>
+  <!-- 问题: 我们不知道什么时候能拿到 max 值, 但总归有一刻, 会得到一个真正的 max 值 -->
+  <!-- 我们可以使用 watch 属性监听, 来监听父组件传递过来的 max 值, 不管 watch 会被触发几次, 但最后一次,肯定是一个 合法的 max 数值 -->
+  <div class="mui-numbox" data-numbox-min='1' data-numbox-max='66'>
     <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-    <input id="test" class="mui-input-numbox" type="number" value="1" />
+    <input id="test" class="mui-input-numbox" type="number" value="1" @change="countChanged" ref="numbox" />
     <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
   </div>
+
+  <!-- 分析: 子组件什么时候把 数据传递给父组件 -->
 </template>
 
 <script>
@@ -13,7 +17,23 @@ export default {
   mounted() {
     // 初始化数字选择框组件
     mui('.mui-numbox').numbox()
+    console.log(this.max);
   },
+  methods: {
+    countChanged() {
+      // 每当 文本框的数据被修改的时候, 立即把最新的数据, 通过事件调用 传递给父组件
+      // console.log(this.$refs.numbox.value);
+      this.$emit('getcount', parseInt(this.$refs.numbox.value))
+    }
+  },
+  props: ['max'],
+  watch: {
+    // 属性监听
+    max: function (newVal, oldVal) {
+      // 使用 JS API 设置 numbox 的最大值
+      mui('.mui-numbox').numbox().setOption('max', newVal)
+    }
+  }
 }
 </script>
 
