63db9d3eb3e3feaa3a0c6dd9e072429c107c8a38
ian <502219968@qq.com>
2018-08-30 21:02:39 +0800

    图文介绍和商品评论页面的渲染

--- src/components/goods/GoodsDesc.vue
+++ src/components/goods/GoodsDesc.vue
@@ -1,15 +1,52 @@
 <template>
-  <div>
-    <h3>商品图文介绍页面</h3>
+  <div class="goodsdesc-container">
+    <h3>{{ info.title }}</h3>
+    <div class="content" v-html="info.content"></div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      id: this.$route.params.id,
+      // info: {},  // 图文数据
+      info: {
+        title: '2018新款MacBook Pro 15.4英寸苹果笔记本电脑',
+        content: '<img src="src/images/mp2018.jpg" alt=""><img src="src/images/mp2.jpg" alt="">'
+      }
+    }
+  },
+  created() {
+    // this.getGoodsDesc()
+  },
+  methods: {
+    getGoodsDesc() {
+      this.$http
+        .get('api/goods/getdesc/' + id)
+        .then(result => {
+          if (result.body.status === 0) {
+            this.info = result.body.message[0]
+          }
+        })
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+.goodsdesc-container {
+  overflow: hidden;
+  padding: 4px;
+  h3 {
+    font-size: 16px;
+    color: #222;
+    margin: 15px 0;
+  }
+  .content {
+    img {
+      width: 100%;
+    }
+  }
+}
 </style>
